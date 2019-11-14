4c965dcc9be3ef3c0f7cda3a544d272ed8c1d857
Grumstukas <tartenaite.toma@gmail.com>
2019-11-14 22:08:36 +0200

    routes and controllers

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("Hi, it's me, new page");
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            "Future page to show the article:
+            $slug"));
+    }
+}
